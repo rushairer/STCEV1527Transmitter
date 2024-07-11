fc89dd37d15e90f7d295461a79fb5e307dbe491e
Abenx <5195693+rushairer@users.noreply.github.com>
2024-07-11 16:34:04 +0800

    feat: README.md

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # STCEV1527Transmitter
+
+基于 STC8 系列的 315/433Mhz+EV1527 发送模块。
+
+VSCode + EIDE + SDCC
+
+使用内部晶振，频率 24Mhz。
+
+```
+T = 400us
+1 = HIGH * 3T + LOW * T
+0 = HIGH * T + LOW * 3T
+Sync = HIGH * T + LOW * 31T
+```
--- src/main.c
+++ src/main.c
@@ -52,18 +52,6 @@ void SendDataBit(unsigned char bit)
     }
 }
 
-void SendEV1527Data(void)
-{
-    // 假设地址码为 E9700，按下了按键 S1
-    SendSync();     // 发送同步起始信号
-    SendDataBit(1); // 发送地址码
-    // 发送功能码（假设按下了 S1）
-    SendDataBit(0);
-    SendDataBit(0);
-    SendDataBit(0);
-    SendDataBit(1);
-}
-
 uint32_t reverseBits(uint32_t number)
 {
     uint32_t reversed = 0;
@@ -84,7 +72,6 @@ void SendEV1527Num(uint32_t num)
     uint8_t i;
     uint32_t result = 24, temp;
     temp            = reverseBits(num);
-    // temp = num;
     SendSync(); // 发送同步起始信号
     while (result) {
         i = temp % 2;
@@ -103,11 +90,6 @@ void main(void)
     P54 = 0;
     P55 = 0;
 
-    // P3M0 &= ~0x09;
-    // P3M1 |= 0x09;
-    // P3PU |= 0x09;
-    // P3IE |= 0x09;
-
     P3M0 = 0x00;
     P3M1 = 0x00;
     P3PU |= 0x09;
@@ -125,15 +107,5 @@ void main(void)
         } else if (P33 == 0) {
             SendEV1527Num(0x350029); // 发送 EV1527 数据
         }
-
-        // if (P30 == 0) {
-        //     if (P33 == 0) {
-        //         SendEV1527Num(0x350049); // 发送 EV1527 数据
-        //     } else {
-        //         SendEV1527Num(0x350019); // 发送 EV1527 数据
-        //     }
-        // } else if (P33 == 0) {
-        //     SendEV1527Num(0x350029); // 发送 EV1527 数据
-        // }
     }
 }
